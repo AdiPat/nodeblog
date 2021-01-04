fbb09cd42e0c9d30e76b0365db4434dfaf46950a
Aditya Patange <adipat247360@gmail.com>
2021-01-04 22:11:55 +0530

    contants: Get NODE_ENV for base url

--- src/constants.js
+++ src/constants.js
@@ -3,14 +3,14 @@ const SERVER_PORT = 8000;
 
 let SERVER_URL = `http://localhost:${SERVER_PORT}`;
 
-if (process.env === "production") {
+if (process.env.NODE_ENV === "production") {
   SERVER_URL = window.location.origin;
 }
 
 const API_ENDPOINTS = {
   posts: (BASE_URL) => `${BASE_URL}/api/posts`,
   post: (BASE_URL, postId) => `${BASE_URL}/api/posts/${postId}`,
-  comments: (BASE_URL, postId) => `${BASE_URL}/api/comments`,
+  comments: (BASE_URL, postId) => `${BASE_URL}/api/${postId}/comments`,
   comment: (BASE_URL, postId, commentId) =>
     `${BASE_URL}/api/posts/${postId}/comments/${commentId}`,
 };
