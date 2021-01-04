d6ce7af3c4597935103b362987e8ae2fdeecf8f0
Aditya Patange <adipat247360@gmail.com>
2021-01-04 19:21:19 +0530

    PostTitle: Add pre-title text

--- src/components/PostTitle.js
+++ src/components/PostTitle.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function PostTitle({ title }) {
-  return <h2 className="post-title">{title}</h2>;
+  return (
+    <div className="post-title-container">
+      <h1 className="post-pretitle">{title.charAt(0)}</h1>
+      <h2 className="post-title">{title}</h2>;
+    </div>
+  );
 }
 
 export { PostTitle };
