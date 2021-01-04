8e82a82ba8719dcc29021c69c4da9de82c8da4b9
Aditya Patange <adipat247360@gmail.com>
2021-01-04 19:01:20 +0530

    PostBody: Render seperate paragraph for each new line

--- src/components/PostBody.js
+++ src/components/PostBody.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 function PostBody({ body }) {
-  return <p className="post-body">{body}</p>;
+  const renderParagraphs = (text) => {
+    const paragraphs = body.split("\n");
+    const paraJsx = paragraphs.map((para) => (
+      <p style={{ marginBottom: "5px" }}>{para}</p>
+    ));
+    return paraJsx;
+  };
+
+  return <div className="post-body">{renderParagraphs(body)}</div>;
 }
 
 export { PostBody };
