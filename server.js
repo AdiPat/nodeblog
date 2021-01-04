79cbd065b9b2e8531f89ea13bef6fe02066db887
Aditya Patange <adipat247360@gmail.com>
2021-01-04 19:59:59 +0530

    Add support to serve react app from express server - Add static files, build folder - Serve static routes from build folder - Append api/ to all api routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const app = express();
 const postsService = require("./server/posts-service");
 const constants = require("./src/constants");
@@ -8,9 +9,14 @@ const { StatusCodes, ReasonPhrases } = require("http-status-codes");
 const port = constants.SERVER_PORT;
 const DEFAULT_USER_ID = 1;
 
+app.use(express.static(path.join(__dirname, "build")));
 app.use(cors());
 
-app.get("/posts", (req, res) => {
+app.get(["/", "/posts", "/posts/:postId"], (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.get("/api/posts", (req, res) => {
   const userId = DEFAULT_USER_ID;
   const posts = postsService.getAllPosts(userId);
   if (!posts) {
@@ -19,7 +25,7 @@ app.get("/posts", (req, res) => {
   return res.json({ data: { userId, posts } });
 });
 
-app.get("/posts/:postId", (req, res) => {
+app.get("/api/posts/:postId", (req, res) => {
   const userId = DEFAULT_USER_ID;
   const postId = parseInt(req.params.postId);
   const post = postsService.getPost(userId, postId);
@@ -29,7 +35,7 @@ app.get("/posts/:postId", (req, res) => {
   return res.json({ data: { userId, post } });
 });
 
-app.get("/posts/:postId/comments", (req, res) => {
+app.get("/api/posts/:postId/comments", (req, res) => {
   const userId = DEFAULT_USER_ID;
   const postId = req.params.postId;
   const comments = postsService.getAllComments(userId, postId);
@@ -39,7 +45,7 @@ app.get("/posts/:postId/comments", (req, res) => {
   return res.json({ data: { userId, comments } });
 });
 
-app.get("/posts/:postId/comments/:commentId", (req, res) => {
+app.get("/api/posts/:postId/comments/:commentId", (req, res) => {
   const userId = DEFAULT_USER_ID;
   const postId = req.params.postId;
   const commentId = req.params.commentId;
--- src/constants.js
+++ src/constants.js
@@ -1,21 +1,23 @@
 const CLIENT_PORT = 3000;
 const SERVER_PORT = 8000;
 
-const CLIENT_URL = `http://localhost:${CLIENT_PORT}`;
-const SERVER_URL = `http://localhost:${SERVER_PORT}`;
+let SERVER_URL = `http://localhost:${SERVER_PORT}`;
+
+if (process.env === "production") {
+  SERVER_URL = window.location.origin;
+}
 
 const API_ENDPOINTS = {
-  posts: (BASE_URL) => `${BASE_URL}/posts`,
-  post: (BASE_URL, postId) => `${BASE_URL}/posts/${postId}`,
-  comments: (BASE_URL, postId) => `${BASE_URL}/comments`,
+  posts: (BASE_URL) => `${BASE_URL}/api/posts`,
+  post: (BASE_URL, postId) => `${BASE_URL}/api/posts/${postId}`,
+  comments: (BASE_URL, postId) => `${BASE_URL}/api/comments`,
   comment: (BASE_URL, postId, commentId) =>
-    `${BASE_URL}/posts/${postId}/comments/${commentId}`,
+    `${BASE_URL}/api/posts/${postId}/comments/${commentId}`,
 };
 
 module.exports = {
   CLIENT_PORT,
   SERVER_PORT,
-  CLIENT_URL,
   SERVER_URL,
   API_ENDPOINTS,
 };
