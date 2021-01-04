abfa51f09c0902d1fcf54bca435abe40ba14a539
Aditya Patange <adipat247360@gmail.com>
2021-01-04 12:16:13 +0530

    server: Remove redundant headers - They are automatically set with res.json()

--- server.js
+++ server.js
@@ -10,21 +10,18 @@ const DEFAULT_USER_ID = 1;
 app.use(cors());
 
 app.get("/posts", (req, res) => {
-  res.setHeader("Content-Type", "application/json");
   const userId = DEFAULT_USER_ID;
   const posts = postsService.getAllPosts(userId);
   return res.json({ data: { userId, posts } });
 });
 
 app.get("/posts/:postId", (req, res) => {
-  res.setHeader("Content-Type", "application/json");
   const userId = DEFAULT_USER_ID;
   const post = postsService.getPost(userId, req.params.postId);
   return res.json({ data: { userId, post } });
 });
 
 app.get("/posts/:postId/comments", (req, res) => {
-  res.setHeader("Content-Type", "application/json");
   const userId = DEFAULT_USER_ID;
   const postId = req.params.postId;
   const comments = postsService.getAllComments(userId, postId);
@@ -32,7 +29,6 @@ app.get("/posts/:postId/comments", (req, res) => {
 });
 
 app.get("/posts/:postId/comments/:commentId", (req, res) => {
-  res.setHeader("Content-Type", "application/json");
   const userId = DEFAULT_USER_ID;
   const postId = req.params.postId;
   const commentId = req.params.commentId;
